14c662beab4ac5b87ccda97d1296e0d0fbfc81eb
savvyh <7277@holbertonstudents.com>
2024-06-26 21:00:41 +0000

    initial commit

--- handball_app/main/admin.py
+++ handball_app/main/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User, Category, TrainingSession, Multimedia, SessionTracking
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(TrainingSession)
+class TrainingSessionAdmin(admin.ModelAdmin):
+    list_display = ('session_title', 'category', 'session_date', 'user')
+    search_fields = ('session_title', 'category', 'user__username')
+    list_filter = ('session_date',)
+
+@admin.register(Multimedia)
+class MultimediaAdmin(admin.ModelAdmin):
+    list_display = ('content_title', 'content_type', 'theme', 'user')
+    search_fields = ('content_title', 'content_type', 'theme', 'user__username')
+    list_filter = ('content_type', 'theme')
+
+@admin.register(SessionTracking)
+class SessionTrackingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'training_session', 'theme')
+    search_fields = ('user__username', 'training_session__session_title', 'theme')
