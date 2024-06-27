e0511ca010a21d72b5fa1fc5748a81057bbe985a
savvyh <7277@holbertonstudents.com>
2024-06-27 15:29:03 +0000

    update the urls from register to signup

--- handball_app/register/urls.py
+++ handball_app/register/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.signup, name='register'),
+    path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('authentification/', views.authentification, name='authentification'),
