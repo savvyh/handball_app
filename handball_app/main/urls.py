8da02fcecbc0121a6cf10a5dd84aef1ce83c5b48
savvyh <7277@holbertonstudents.com>
2024-06-30 22:23:32 +0000

    Update the urls of the main application. Now, home page depends on the user profile

--- handball_app/main/urls.py
+++ handball_app/main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -7,7 +7,8 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('subscribe/', views.subscribe, name='subscribe'),
     path('settings/', views.settings, name='settings'),
-    path('account/', views.account, name='account'),
+    path('personal_space/<int:profile_id>/', views.personal_space, name='personal_space'),
     path('club/', views.club, name='club'),
     path('create-training/', views.create_training, name='create-training'),
+    path('profiles/create/', views.create_profile, name='create_profile'),
 ]
