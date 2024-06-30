7d3d3bdb9f990285fa2bd6995fa77c25ce301520
savvyh <7277@holbertonstudents.com>
2024-06-30 22:22:52 +0000

    update views of the main application tp handle the home page and user profil creation

--- handball_app/main/views.py
+++ handball_app/main/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from .models import Profile, CATEGORY_CHOICES
+from .forms import ProfileCreationForm
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 def landing(request):
     return render(request, 'main/landing.html')
 
+@login_required
 def home(request):
-    return render(request, 'main/home.html')
+    profiles = Profile.objects.filter(user=request.user)
+    return render(request, 'main/home.html', {'profiles': profiles})
 
 def subscribe(request):
     return render(request, 'main/subscribe.html')
 
-def account(request):
-    return render(request, 'main/account.html')
+@login_required
+def personal_space(request, profile_id):
+    profile = Profile.objects.get(id=profile_id, user=request.user)
+    return render(request, 'main/personal_space.html', {'profile': profile})
 
 def club(request):
     return render(request, 'main/club.html')
@@ -20,3 +30,47 @@ def settings(request):
 
 def create_training(request):
     return render(request, 'main/create-training.html')
+
+@login_required
+def create_profile(request):
+    if not request.user.can_add_profile():
+        return redirect('home')
+    if request.method == 'POST':
+        form = ProfileCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            selected_categories = request.POST.getlist('categories')
+            profile.categories.set(selected_categories)
+            request.user.profile_completed = True
+            request.user.save()
+            return redirect('home')
+    else:
+        form = ProfileCreationForm()
+    categories = CATEGORY_CHOICES
+    return render(request, 'main/create_profile.html', {'form': form, 'categories': categories})
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    template_name = 'main/create_profile.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        response = super().form_valid(form)
+        selected_categories = self.request.POST.getlist('categories')
+        self.object.categories.set(selected_categories)
+        self.object.save()
+        self.request.user.profile_completed = True
+        self.request.user.save()
+        return response
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'main/profile_list.html'
+    context_object_name = 'profiles'
+
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
