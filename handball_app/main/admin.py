ad9900efd5905592ae0e444a67e36dcb9dd602fc
savvyh <7277@holbertonstudents.com>
2024-07-03 12:23:05 +0000

    Update the admin file to display models

--- handball_app/main/admin.py
+++ handball_app/main/admin.py
@@ -1,29 +1,10 @@
 from django.contrib import admin
-from .models import User, Category, TrainingSession, Multimedia, SessionTracking
+from .models import User, Category, TrainingSession, Multimedia, SessionTracking, Personalisation
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_superuser')
-    search_fields = ('username', 'email')
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-@admin.register(TrainingSession)
-class TrainingSessionAdmin(admin.ModelAdmin):
-    list_display = ('session_title', 'category', 'session_date', 'user')
-    search_fields = ('session_title', 'category', 'user__username')
-    list_filter = ('session_date',)
-
-@admin.register(Multimedia)
-class MultimediaAdmin(admin.ModelAdmin):
-    list_display = ('content_title', 'content_type', 'theme', 'user')
-    search_fields = ('content_title', 'content_type', 'theme', 'user__username')
-    list_filter = ('content_type', 'theme')
-
-@admin.register(SessionTracking)
-class SessionTrackingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'training_session', 'theme')
-    search_fields = ('user__username', 'training_session__session_title', 'theme')
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(TrainingSession)
+admin.site.register(Multimedia)
+admin.site.register(SessionTracking)
+admin.site.register(Personalisation.Exercice)
+admin.site.register(Personalisation.Theme)
